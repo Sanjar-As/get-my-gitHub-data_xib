1d194892508dc4ad6d9a0eae9b33a4c7c33b947e
Sanjar <aslonovsanjar02@gmail.com>
2022-05-06 14:58:01 +0500

    Initial Commit

---
+++ module_8.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  module_8.1.1
+//
+//  Created by Sanjar Aslonov on 06/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
